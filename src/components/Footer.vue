52ac236e12bd4bca39ec4b05de8bd87a02cc5849
unknown <nuraziddin201@gmail.com>
2023-03-08 09:34:51 +0700

    satuan

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+<footer class="footer footer-black  footer-white ">
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="footer-nav">
+          <ul>
+            <li><a href="https://www.creative-tim.com" target="_blank">Creative Tim</a></li>
+            <li><a href="https://www.creative-tim.com/blog" target="_blank">Blog</a></li>
+            <li><a href="https://www.creative-tim.com/license" target="_blank">Licenses</a></li>
+          </ul>
+        </nav>
+        <div class="credits ml-auto">
+          <span class="copyright">
+            ©2023 made with <i class="fa fa-heart heart"></i> by Creative Tim
+          </span>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import
+
+</script>
+
+
+<style>
+#app (
+    font-family: Avenir, Helvetica,  arial, sans-serif;
+    :-webkit-font-smoothing: antialiased;
+    :-moz-asx-font-smoothing: grayscale;
+    text-align: center;
+    color
+)
+
+</style>
